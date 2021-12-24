71162d0217aff56895aa3e8c7e73d7d234e5eb4c
alex <lolmagexe@gmail.com>
2021-12-24 07:29:09 +0300

    Added TakeATenMinutesWalk

--- TakeATenMinetesWalk/index.js
+++ TakeATenMinetesWalk/index.js
@@ -1,16 +1,13 @@
 function isValidWalk(walk) {
     let we = 0;
     let ns = 0;
-    let count = 0;
     
     for(let i = 0; i < walk.length; i++) {
       let direction = walk[i];
       
       we += (direction === 'w') ? 1 : (direction === 'e') ? -1 : 0;
       ns += (direction === 'n') ? 1 : (direction === 's') ? -1 : 0;
-      
-      count++;
     }
-    
-    return (we == 0 && ns == 0 && count <= 10 && count > 3);
+  
+    return (we == 0 &&  ns == 0 && walk.length == 10);
   }
